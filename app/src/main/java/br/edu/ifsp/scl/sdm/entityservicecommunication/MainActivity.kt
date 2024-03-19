9e5e03e0248ce900bd389dfb42f7a849b5bed8bf
Bleiner Mathias <bleinerbbm@hotmail.com>
2024-03-19 14:21:38 -0300

    Refactor(Service): Implement Broadcast Receiver

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -1,6 +1,9 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunication
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var incrementServiceIntent : Intent
     private var counter = 0
+
+    private val incrementBroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            intent.getIntExtra("VALUE", -1)?.also {value->
+                counter= value
+                Toast.makeText(this@MainActivity, "You clicked $counter times", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,11 +50,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        // Ouvir os dados
-        InterEntityCommunication.valueLiveData.observe(this) {value ->
-            counter = value
-            Toast.makeText(this, "You clicked $counter times.", Toast.LENGTH_SHORT).show()
-        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(incrementBroadcastReceiver, IntentFilter("INCREMENT_VALUE_ACTION"))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(incrementBroadcastReceiver)
     }
 
     // Método para destrutir o serviço
